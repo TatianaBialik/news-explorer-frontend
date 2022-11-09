cce69c96c270cb1f6b1bdceebcd8307bf6925afc
Tatiana Bialik <bialik.tat@gmail.com>
2022-11-09 23:03:54 +0200

    feat: burger menu for mobile version add

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,18 +1,51 @@
 import './Navigation.css';
 import { useLocation, NavLink } from 'react-router-dom';
+import { useState } from 'react';
 import logout_black from '../../images/logout_black.svg';
 import logout_white from '../../images/logout_white.svg';
 
 function Navigation({ isLoggedIn, username }) {
   const location = useLocation();
   const navClass = location.pathname === '/' ? '' : '_saved';
+  const [isBurgerButtonActive, setIsBurgerButtonActive] = useState(false);
 
   return (
     <div className="navigation">
-      <p className={`navigation__logo navigation__logo` + navClass}>NewsExplorer</p>
+      {/* MOBILE NAVIGATION MENU */}
+      {isBurgerButtonActive ? (
+        /* OPEN MOBILE NAVIGATION MENU */
+        <div className="navigation__burger">
+          <div className="navigation__top">
+            <p className={`navigation__logo navigation__logo`}>NewsExplorer</p>
+            <button className="navigation__close-menu-button" />
+          </div>
+          
+          <div className="navigation__links_active">
+            <NavLink to='/' className={`navigation__link navigation__link`}>Home</NavLink>
+            <NavLink to='/saved' className={`navigation__link navigation__link`}>Saved articles</NavLink>
+
+            { isLoggedIn ? (
+              <button className={`navigation__header-button navigation__header-button_logout navigation__header-button`}>
+                { username }
+                <img src={ logout_white } />
+              </button>
+              ) : (
+                <button className={`navigation__header-button navigation__header-button_login navigation__header-button`}>Sign in</button>
+              )}
+          </div>
+        </div>
+      ) : (
+        /* CLOSE MOBILE NAVIGATION MENU (BURGER) */
+        <>
+          <p className={`navigation__logo navigation__logo` + navClass}>NewsExplorer</p>
+          <button className={`navigation__burger-menu navigation__burger-menu` + navClass} />
+        </>
+      )}
+
+      {/* REGULAR NAVIGATION MENU */}
       <nav className="navigation__links">
-        <NavLink to='/' className={`navigation__link navigation__link` + navClass}>Home</NavLink>
-        <NavLink to='/saved' className={`navigation__link navigation__link` + navClass}>Saved articles</NavLink>
+        <NavLink to='/' className={`${navClass === '' && 'navigation__link_location'} navigation__link navigation__link` + navClass}>Home</NavLink>
+        <NavLink to='/saved' className={`${navClass === '_saved' && 'navigation__link_location'} navigation__link navigation__link` + navClass}>Saved articles</NavLink>
         { isLoggedIn ? (
           <button className={`navigation__header-button navigation__header-button_logout navigation__header-button` + navClass}>
             { username }
@@ -23,7 +56,6 @@ function Navigation({ isLoggedIn, username }) {
         )}
       </nav>
     </div>
-    
   )
 };
 
